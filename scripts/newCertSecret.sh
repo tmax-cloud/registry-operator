e9cc3a43517482994fa4af442f662701c1fdfaa9
sukhee_yun <sukhee_yun@tmax.co.kr>
2020-10-14 16:17:04 +0900

    [mod] scripts/newCertSecret.sh

--- scripts/newCertSecret.sh
+++ scripts/newCertSecret.sh
@@ -1,3 +1,12 @@
 #/bin/sh
 
-kubectl create secret generic registry-ca --from-file=ca.crt=./ca.crt --from-file=ca.key=./ca.key -n hypercloud4-system
+CA_CRT=./ca.crt
+CA_KEY=./ca.key
+
+if [ -n "$1" ] && [ -n "$2" ]; then
+    CA_CRT=$1
+    CA_KEY=$2
+fi
+
+echo "Create CA($CA_CRT, $CA_KEY) registry-ca secret"
+kubectl create secret generic registry-ca --from-file=ca.crt=${CA_CRT} --from-file=ca.key=${CA_KEY} -n registry-system
