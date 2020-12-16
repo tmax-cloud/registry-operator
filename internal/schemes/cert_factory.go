3ac25813f36e4948aea52796902a1eab6558eafa
sukhee_yun <sukhee_yun@tmax.co.kr>
2020-12-16 14:49:35 +0900

    [feat] implement certificate factory

---
+++ internal/schemes/cert_factory.go
@@ -0,0 +1,185 @@
+package schemes
+
+import (
+	"crypto/x509/pkix"
+	"fmt"
+	"net"
+
+	regv1 "github.com/tmax-cloud/registry-operator/api/v1"
+	"github.com/tmax-cloud/registry-operator/internal/utils"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+var schemeLogger = logf.Log.WithName("scheme")
+
+const (
+	TLSCert = "tls.crt"
+	TLSKey  = "tls.key"
+)
+
+// Certificate is to compose x509 certificate's template
+type Certificate interface {
+	GetSanIP(interface{}) []net.IP
+	GetSanDNS(interface{}) []string
+	GetSubject() *pkix.Name
+}
+
+// CertFactory is to make CertPair
+type CertFactory struct {
+	client client.Client
+}
+
+// CertType is a certificate pair type to create
+type CertType int
+
+const (
+	certTypeNotaryServer CertType = iota
+	certTypeNotarySigner
+)
+
+// NewCertFactory is ...
+func NewCertFactory(c client.Client) *CertFactory {
+	return &CertFactory{
+		client: c,
+	}
+}
+
+// CreateCertPair is to create new CertPair of the types you want
+func (f *CertFactory) CreateCertPair(source interface{}, certType CertType) (*utils.CertPair, error) {
+	var out *utils.CertPair
+	ct := &CertTemplate{}
+
+	switch certType {
+	case certTypeNotaryServer:
+		pair, err := ct.CreateCertPair(&NotaryServerCert{}, source)
+		if err != nil {
+			return nil, err
+		}
+		out = pair
+
+	case certTypeNotarySigner:
+		pair, err := ct.CreateCertPair(&NotarySignerCert{}, source)
+		if err != nil {
+			return nil, err
+		}
+		out = pair
+	default:
+		return nil, fmt.Errorf("not supported cert type")
+	}
+
+	f.setParent(out)
+	if err := f.createCertificateData(out); err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
+
+func (f *CertFactory) setParent(cert *utils.CertPair) {
+	parentCert, parentPrivKey := getCertificateFromFile(f.client)
+	cert.SetParent(parentCert, parentPrivKey)
+}
+
+func (f *CertFactory) createCertificateData(cert *utils.CertPair) error {
+	if err := cert.CreateCertificateData(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// CertTemplate is CertPair's template
+type CertTemplate struct{}
+
+func (t *CertTemplate) CreateCertPair(cert Certificate, source interface{}) (*utils.CertPair, error) {
+	out, err := utils.NewCertPair(nil, false)
+	if err != nil {
+		return nil, err
+	}
+
+	out.SetSubject(cert.GetSubject())
+	out.SetSubjectAltName(cert.GetSanIP(source), cert.GetSanDNS(source))
+
+	return out, nil
+}
+
+// NotaryServerCert is Notary Server's Certificate
+type NotaryServerCert struct{}
+
+func (n *NotaryServerCert) GetSanIP(notary interface{}) []net.IP {
+	nt := notary.(*regv1.Notary)
+
+	ips := []net.IP{}
+	if len(nt.Status.ServerClusterIP) > 0 {
+		ips = append(ips, net.ParseIP(nt.Status.ServerClusterIP))
+	}
+	if len(nt.Status.ServerLoadBalancerIP) > 0 {
+		ips = append(ips, net.ParseIP(nt.Status.ServerLoadBalancerIP))
+	}
+
+	return ips
+}
+
+func (n *NotaryServerCert) GetSanDNS(notary interface{}) []string {
+	nt := notary.(*regv1.Notary)
+
+	domains := []string{}
+	if nt.Spec.ServiceType == "Ingress" {
+		// domains = append(domains, getNotaryIngressDomain())
+	}
+	domains = append(domains, utils.BuildServiceHostname(SubresourceName(nt, SubTypeNotaryServerService), nt.Namespace))
+
+	return domains
+}
+
+func (n *NotaryServerCert) GetSubject() *pkix.Name {
+	subject := &pkix.Name{
+		Country:       []string{"KR"},
+		Organization:  []string{"tmax"},
+		StreetAddress: []string{"Seoul"},
+		CommonName:    "notary-server.tmax.co.kr",
+	}
+
+	return subject
+}
+
+// NotarySignerCert is Notary Signer's Certificate
+type NotarySignerCert struct{}
+
+func (n *NotarySignerCert) GetSanIP(notary interface{}) []net.IP {
+	nt := notary.(*regv1.Notary)
+
+	ips := []net.IP{}
+	if len(nt.Status.SignerClusterIP) > 0 {
+		ips = append(ips, net.ParseIP(nt.Status.SignerClusterIP))
+	}
+	if len(nt.Status.SignerLoadBalancerIP) > 0 {
+		ips = append(ips, net.ParseIP(nt.Status.SignerLoadBalancerIP))
+	}
+
+	return ips
+}
+
+func (n *NotarySignerCert) GetSanDNS(notary interface{}) []string {
+	nt := notary.(*regv1.Notary)
+
+	domains := []string{}
+	if nt.Spec.ServiceType == "Ingress" {
+		// domains = append(domains, getNotaryIngressDomain())
+	}
+	domains = append(domains, utils.BuildServiceHostname(SubresourceName(nt, SubTypeNotarySignerService), nt.Namespace))
+
+	return domains
+}
+
+func (n *NotarySignerCert) GetSubject() *pkix.Name {
+	subject := &pkix.Name{
+		Country:       []string{"KR"},
+		Organization:  []string{"tmax"},
+		StreetAddress: []string{"Seoul"},
+		CommonName:    "notary-signer.tmax.co.kr",
+	}
+
+	return subject
+}
---
+++ internal/schemes/util.go
@@ -0,0 +1,101 @@
+package schemes
+
+import (
+	"context"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+
+	regv1 "github.com/tmax-cloud/registry-operator/api/v1"
+	"github.com/tmax-cloud/registry-operator/internal/utils"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type SubresourceType int
+
+const (
+	NotaryServerPrefix = "server-"
+	NotarySignerPrefix = "signer-"
+	NotaryDBPrefix     = "db-"
+)
+
+const (
+	SubTypeNotaryDBPod = SubresourceType(iota)
+	SubTypeNotaryDBPVC
+	SubTypeNotaryDBService
+	SubTypeNotaryServerIngress
+	SubTypeNotaryServerPod
+	SubTypeNotaryServerSecret
+	SubTypeNotaryServerService
+	SubTypeNotarySignerPod
+	SubTypeNotarySignerSecret
+	SubTypeNotarySignerService
+)
+
+// SubresourceName returns Notary's or Registry's subresource name
+func SubresourceName(subresource interface{}, subresourceType SubresourceType) string {
+	switch res := subresource.(type) {
+	case *regv1.Notary:
+		switch subresourceType {
+		// Notary DB
+		case SubTypeNotaryDBPod, SubTypeNotaryDBPVC, SubTypeNotaryDBService:
+			return regv1.K8sPrefix + regv1.K8sNotaryPrefix + NotaryDBPrefix + res.Name
+
+		// Notary Server
+		case SubTypeNotaryServerIngress, SubTypeNotaryServerPod, SubTypeNotaryServerSecret, SubTypeNotaryServerService:
+			return regv1.K8sPrefix + regv1.K8sNotaryPrefix + NotaryServerPrefix + res.Name
+
+		// Notary signer
+		case SubTypeNotarySignerPod, SubTypeNotarySignerSecret, SubTypeNotarySignerService:
+			return regv1.K8sPrefix + regv1.K8sNotaryPrefix + NotarySignerPrefix + res.Name
+		}
+
+		// TODO: *regv1.Registry
+	}
+
+	return ""
+}
+
+const (
+	RootCASecretName      = "registry-ca"
+	RootCASecretNamespace = "registry-system"
+)
+
+func getCertificateFromFile(c client.Client) (*x509.Certificate, *rsa.PrivateKey) {
+	logger := utils.GetRegistryLogger(corev1.Secret{}, "CertScheme", "secret")
+
+	rootSecret := corev1.Secret{}
+	req := types.NamespacedName{Name: RootCASecretName, Namespace: RootCASecretNamespace}
+	if err := c.Get(context.TODO(), req, &rootSecret); err != nil {
+		logger.Error(err, "Get Root Secret Error")
+		return nil, nil
+	}
+
+	block, rest := pem.Decode(rootSecret.Data[RootCACert])
+	if len(rest) != 0 {
+		logger.Info("Cert is not PEM format", "Rest", rest)
+		return nil, nil
+	}
+
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		logger.Error(err, "Parse Root CA block Error")
+		return nil, nil
+	}
+
+	privBlock, privRest := pem.Decode(rootSecret.Data[RootCAPriv])
+	if len(privRest) != 0 {
+		logger.Info("Private key is not PEM format", "Rest", privRest)
+		return nil, nil
+	}
+
+	key, privKeyErr := x509.ParsePKCS8PrivateKey(privBlock.Bytes)
+	if privKeyErr != nil {
+		logger.Error(privKeyErr, "Parse private key Error")
+		return nil, nil
+	}
+
+	return cert, key.(*rsa.PrivateKey)
+}
