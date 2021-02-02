50781280fba02a51b9a7dec6bfda1225e48993cb
taejune_ahn <taejune_ahn@tmax.co.kr>
2021-02-02 09:51:21 +0900

    [refactor] refactor registry_scan

--- controllers/scanctl/registry_scan.go
+++ controllers/scanctl/registry_scan.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"context"
 	"crypto/tls"
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -13,26 +12,42 @@ import (
 	"strings"
 	"time"
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 	"github.com/cloudflare/cfssl/log"
 
 =======
 	
 >>>>>>> Refactor GetVulnerability
+=======
+
+	"github.com/cloudflare/cfssl/log"
+
+>>>>>>> [refactor] refactor registry_scan
 	"github.com/genuinetools/reg/clair"
-	reg "github.com/genuinetools/reg/clair"
 	"github.com/genuinetools/reg/registry"
 	"github.com/genuinetools/reg/repoutils"
+<<<<<<< HEAD
 	tmaxiov1 "github.com/tmax-cloud/registry-operator/api/v1"
 	"github.com/tmax-cloud/registry-operator/internal/common/certs"
 	regConfig "github.com/tmax-cloud/registry-operator/internal/common/config"
 	"github.com/tmax-cloud/registry-operator/internal/utils"
 	regApi "github.com/tmax-cloud/registry-operator/pkg/registry"
 	clairReg "github.com/tmax-cloud/registry-operator/pkg/scan/clair"
+=======
+	corev1 "k8s.io/api/core/v1"
+>>>>>>> [refactor] refactor registry_scan
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
+
+	tmaxiov1 "github.com/tmax-cloud/registry-operator/api/v1"
+	"github.com/tmax-cloud/registry-operator/internal/auth"
+	regConfig "github.com/tmax-cloud/registry-operator/internal/common/config"
+	"github.com/tmax-cloud/registry-operator/internal/utils"
+	regApi "github.com/tmax-cloud/registry-operator/pkg/registry"
+	clairReg "github.com/tmax-cloud/registry-operator/pkg/scan/clair"
 )
 
 var logger = logf.Log.WithName("registry_scan")
@@ -42,7 +57,7 @@ var (
 	Priorities = []string{"Unknown", "Negligible", "Low", "Medium", "High", "Critical", "Defcon1"}
 )
 
-func ParseAnalysis(threshold int, report *reg.VulnerabilityReport) (map[string]int, []string, map[string]tmaxiov1.Vulnerabilities) {
+func ParseAnalysis(threshold int, report *clair.VulnerabilityReport) (map[string]int, []string, map[string]tmaxiov1.Vulnerabilities) {
 	vulnerabilities := make(map[string]tmaxiov1.Vulnerabilities)
 	summary := make(map[string]int)
 	var fatal []string
@@ -109,16 +124,26 @@ func ParseAnalysis(threshold int, report *reg.VulnerabilityReport) (map[string]i
 	return summary, fatal, vulnerabilities
 }
 
-func InitParameter(target *tmaxiov1.ScanTarget) {
-	if target.TimeOut == 0 {
-		target.TimeOut = time.Minute
+func getCertificateFromSecret(secretName, namespace string) ([]byte, error) {
+
+<<<<<<< HEAD
+=======
+	secret, err := getSecret(secretName, namespace)
+	if err != nil {
+		return nil, errors.NewBadRequest("Cannot find secret named" + secretName + " from " + namespace)
 	}
+
+	if secret.Type == corev1.SecretTypeTLS {
+		return secret.Data["tls.crt"], nil
+	} else if secret.Type == corev1.SecretTypeOpaque {
+		logger.Info("[WARN]: Certificate Secret for registry client is used as opaque type. (Using TLS type recommended)")
+		return secret.Data["ca.crt"], nil
+	}
+
+	return nil, errors.NewBadRequest("Only TLS Secret is allowed.")
 }
 
-func imageFullPath(registryUrl, image string) string {
-	return path.Join(registryUrl, image)
-}
-
+>>>>>>> [refactor] refactor registry_scan
 func GetRegistryImages(c client.Client, registryURL, basicAuth, imageNamePattern, certSecret, namespace string) []string {
 	images := []string{}
 	var ca []byte
@@ -127,7 +152,11 @@ func GetRegistryImages(c client.Client, registryURL, basicAuth, imageNamePattern
 	if certSecret != "" {
 		var err error
 
+<<<<<<< HEAD
 		ca, err = utils.GetCAData(certSecret, namespace)
+=======
+		ca, err = getCertificateFromSecret(certSecret, namespace)
+>>>>>>> [refactor] refactor registry_scan
 		if err != nil {
 			logger.Error(err, "failed to get ca")
 			return images
@@ -156,6 +185,7 @@ func GetRegistryImages(c client.Client, registryURL, basicAuth, imageNamePattern
 }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 func GetVulnerability(c client.Client, instance *tmaxiov1.ImageScanRequest) (map[string]map[string]*reg.VulnerabilityReport, error) {
 	reports := map[string]map[string]*reg.VulnerabilityReport{}
 
@@ -167,32 +197,38 @@ func GetVulnerability(c client.Client, instance *tmaxiov1.ImageScanRequest) (map
 func getBasicAuth(imagePullSecret, namespace, registryURL string) (string, error) {
 	logger.Info("Get " + imagePullSecret + "secret from " + namespace)
 	secret, err := getSecret(imagePullSecret, namespace)
+=======
+func getBasicAuthTokenFromImagePullSecret(secretName, namespace, registryURL string) (string, error) {
+	//
+	secret, err := getSecret(secretName, namespace)
+>>>>>>> [refactor] refactor registry_scan
 	if err != nil {
-		logger.Error(err, "failed to get image pull secret")
+		logger.Error(err, "Failed to get imagePullSecret from "+namespace)
 		return "", err
 	}
 
+	// build basicAuth
 	basic, err := utils.ParseBasicAuth(secret, registryURL)
 	if err != nil {
 		logger.Error(err, "failed to parse basic auth")
 		return "", err
 	}
 
+	logger.Info("Parsed basic: " + basic)
 	return basic, nil
 }
 
-
 func setImageNames(c client.Client, image string, imagePullSecret string, certificateSecret string, namespace string, registryURL string) ([]string, error) {
 	var entries = []string{}
-			
+
 	if strings.Contains(image, "*") || strings.Contains(image, "?") {
 		var basicAuth string
-		
+
 		if imagePullSecret == "" {
 			return entries, errors.NewBadRequest("Image(" + image + ")'s ImagePullSecret not provided.")
 		}
-		
-		basic, err := getBasicAuth(imagePullSecret, namespace, registryURL)
+
+		basic, err := getBasicAuthTokenFromImagePullSecret(imagePullSecret, namespace, registryURL)
 		if err != nil {
 			return entries, errors.NewBadRequest("Failed to get basic auth from imagePullSecret" + imagePullSecret)
 		}
@@ -205,24 +241,55 @@ func setImageNames(c client.Client, image string, imagePullSecret string, certif
 
 	return entries, nil
 }
+func fetchImageList(reg *registry.Registry, images []string) ([]string, error) {
 
-func GetVulnerability(c client.Client, o *tmaxiov1.ImageScanRequest) (map[string]map[string]*reg.VulnerabilityReport, error) {
-	
-	reports := map[string]map[string]*reg.VulnerabilityReport{}
+	var ret = []string{}
 
-	// clair address 
-	scannerAddr := regConfig.Config.GetString("clair.url")
-	if len(scannerAddr) == 0 {
-		return reports, errors.NewBadRequest("Cannot get address of Clair server.")
->>>>>>> Refactor GetVulnerability
+	for _, entry := range images {
+		if strings.Contains(entry, "*") {
+			reg.Catalog()
+		} else if strings.Contains(entry, "?") {
+
+		} else {
+			ret = append(ret, entry)
+		}
 	}
 
-	for i, target := range o.Spec.ScanTargets {
-		InitParameter(&o.Spec.ScanTargets[i])
-		if target.FixableThreshold < 0 {
-			return reports, errors.NewBadRequest("fixable threshold must be a positive integer")
+	return ret, nil
+}
+
+func GetVulnerability(c client.Client, o *tmaxiov1.ImageScanRequest) (map[string]map[string]*clair.VulnerabilityReport, error) {
+
+	results := map[string]map[string]*clair.VulnerabilityReport{}
+
+	scannerAddr := regConfig.Config.GetString("clair.url")
+	if len(scannerAddr) == 0 {
+<<<<<<< HEAD
+		return reports, errors.NewBadRequest("Cannot get address of Clair server.")
+>>>>>>> Refactor GetVulnerability
+=======
+		return results, errors.NewBadRequest("Cannot get address of Clair server.")
+>>>>>>> [refactor] refactor registry_scan
+	}
+
+	for _, scanTarget := range o.Spec.ScanTargets {
+		if scanTarget.TimeOut == 0 {
+			scanTarget.TimeOut = time.Minute
 		}
 
+		if len(scanTarget.RegistryURL) < 1 {
+			return nil, errors.NewBadRequest("Empty registry URL")
+		} else if len(scanTarget.ImagePullSecret) < 1 {
+			return nil, errors.NewBadRequest("Empty ImagePullSecret")
+		} else if len(scanTarget.CertificateSecret) < 1 {
+			return nil, errors.NewBadRequest("Empty CertificateSecret")
+		} else if !scanTarget.ForceNonSSL && strings.HasPrefix(config.ServerAddress, "http:") {
+			return nil, errors.NewBadRequest("attempted to use insecure protocol! Use force-non-ssl option to force")
+		} else if scanTarget.FixableThreshold < 0 {
+			return results, errors.NewBadRequest("fixable threshold must be a positive integer")
+		}
+
+<<<<<<< HEAD
 		for _, targetImage := range target.Images {
 <<<<<<< HEAD
 			matchImages := []string{}
@@ -241,18 +308,35 @@ func GetVulnerability(c client.Client, o *tmaxiov1.ImageScanRequest) (map[string
 				matchImages = append(matchImages, targetImage)
 =======
 			imageNames, err := setImageNames(c, targetImage, target.ImagePullSecret, target.CertificateSecret, o.Namespace, target.RegistryURL)
+=======
+		dockerLoginSecret := getSecret(scanTarget.ImagePullSecret, namespace)
+		dockerLogin := auth.NewLoginProvider(dockerLoginSecret)
+
+		caSecret := getSecret(scanTarget.CertificateSecret, namespace)
+		ca := auth.NewCertProvider(caSecret)
+
+		var registryAddr string
+		if len(scanTarget.AuthURL) > 0 {
+			registryAddr = scanTarget.AuthURL
+		} else {
+			registryAddr = scanTarget.RegistryURL
+		}
+
+		regClient := newRegClient(registryAddr, dockerLogin, ca)
+
+		for _, scanTargetImage := range scanTarget.Images {
+			imageNames, err := setImageNames(c, scanTargetImage, scanTarget.ImagePullSecret, scanTarget.CertificateSecret, o.Namespace, scanTarget.RegistryURL)
+>>>>>>> [refactor] refactor registry_scan
 			if err != nil {
 				logger.Error(err, "Failed to set image entries.")
 >>>>>>> Refactor GetVulnerability
 			}
 
 			for _, name := range imageNames {
-				imgUrl := imageFullPath(target.RegistryURL, name)
-				logger.Info(fmt.Sprintf("scan image: %s", imgUrl))
-				
-				image, err := registry.ParseImage(imgUrl)
+				image, err := registry.ParseImage(path.Join(scanTarget.RegistryURL, name))
 				if err != nil {
 					logger.Error(err, "failed to parse image")
+<<<<<<< HEAD
 					return reports, err
 				}
 
@@ -280,23 +364,56 @@ func GetVulnerability(c client.Client, o *tmaxiov1.ImageScanRequest) (map[string
 				if report, err = cr.Vulnerabilities(context.TODO(), r, image.Path, image.Reference()); err != nil {
 					logger.Error(err, "failed to get image vulnerabilities")
 					return reports, err
+=======
+					return results, err
+>>>>>>> [refactor] refactor registry_scan
 				}
-
-				// set report
-				if m, ok := reports[target.RegistryURL]; ok {
-					m[name] = &report
-					reports[target.RegistryURL] = m
-				} else {
-					reports[target.RegistryURL] = map[string]*reg.VulnerabilityReport{name: &report}
-				}
-
+				logger.Info("[Parsed image]: Domain: " + image.Domain + " /Path: " + image.Path + " /Tag: " + image.Tag)
+				targets.append(image)
 			}
 		}
 	}
 
-	return reports, nil
+	// for _, scanTarget := range targets {
+	// 			// Initialize clair client.
+	// 			cr, err := clair.New(scannerAddr, clair.Opt{
+	// 				Debug:    scanTarget.Debug,
+	// 				Timeout:  scanTarget.TimeOut,
+	// 				Insecure: scanTarget.Insecure,
+	// 			})
+	// 			if err != nil {
+	// 				logger.Error(err, "failed to new clair client")
+	// 				return results, err
+	// 			}
+
+	// 			report := clair.VulnerabilityReport{}
+
+	// 			// Get the vulnerability report.
+	// 			if report, err = cr.Vulnerabilities(context.TODO(), r, image.Path, image.Reference()); err != nil {
+	// 				logger.Info(err)
+	// 				// Fallback to Clair v3 API.
+	// 				if report, err = cr.VulnerabilitiesV3(context.TODO(), r, image.Path, image.Reference()); err != nil {
+	// 					logger.Error(err, "failed to check vulnerabilities")
+	// 					return results, err
+	// 				}
+	// 			}
+
+	// 			// set report
+	// 			if m, ok := results[scanTarget.RegistryURL]; ok {
+	// 				m[name] = &report
+	// 				results[scanTarget.RegistryURL] = m
+	// 			} else {
+	// 				results[scanTarget.RegistryURL] = map[string]*clair.VulnerabilityReport{name: &report}
+	// 			}
+
+	// 		}
+	// 	}
+	// }
+
+	return results, nil
 }
 
+<<<<<<< HEAD
 func createRegistryClient(target *tmaxiov1.ScanTarget, domain, namespace string) (*registry.Registry, error) {
 	var ca []byte
 	username, password := "", ""
@@ -352,25 +469,37 @@ func createRegistryClient(target *tmaxiov1.ScanTarget, domain, namespace string)
 
 	// get auth config
 	auth, err := repoutils.GetAuthConfig(username, password, authDomain)
+=======
+func getSecret(name, namespace string) (*corev1.Secret, error) {
+	c, err := client.New(config.GetConfigOrDie(), client.Options{})
 	if err != nil {
-		logger.Error(err, "failed to get auth config")
+		return nil, err
+	}
+	secret := &corev1.Secret{}
+	if err := c.Get(context.TODO(), types.NamespacedName{Name: name, Namespace: namespace}, secret); err != nil {
 		return nil, err
 	}
 
-	// Prevent non-ssl unless explicitly forced
-	if !target.ForceNonSSL && strings.HasPrefix(auth.ServerAddress, "http:") {
-		return nil, fmt.Errorf("attempted to use insecure protocol! Use force-non-ssl option to force")
+	return secret, nil
+}
+
+func newRegClient(registryUrl string, login *auth.LoginProvider, cert *auth.CertProvider) (*registry.Registry, error) {
+
+	config, err := repoutils.GetAuthConfig(login.getID(), login.getPassword(), registryUrl)
+>>>>>>> [refactor] refactor registry_scan
+	if err != nil {
+		logger.Error(err, "Failed to get auth config")
+		return nil, err
 	}
 
-	// Create the registry client.
-	return clairReg.New(context.TODO(), auth, registry.Opt{
-		Domain:   domain,
-		Insecure: target.Insecure,
-		Debug:    target.Debug,
-		SkipPing: target.SkipPing,
-		NonSSL:   target.ForceNonSSL,
-		Timeout:  target.TimeOut,
-	}, ca)
+	// TODO: Set opt from operator's registry config.
+	return clairReg.New(context.TODO(), config, registry.Opt{
+		Insecure: false,
+		Debug:    true,
+		SkipPing: true,
+		NonSSL:   false,
+		Timeout:  time.Minute,
+	}, cert.getCert())
 }
 
 func SendElasticSearchServer(url string, namespace string, name string, body *tmaxiov1.ImageScanRequestESReport) (resp *http.Response, err error) {
