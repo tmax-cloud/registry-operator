b43176722fe7134233079ac8cf0a6a4e42b0d08e
yshcoffee <yshcoffee@gmail.com>
2021-02-10 12:34:55 +0900

    [fix] update condition

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -43,6 +43,8 @@ func SetCondition(error error, patch interface{}, condition *status.Condition) {
 		p.Status.Conditions.SetCondition(*condition)
 	case *regv1.Notary:
 		p.Status.Conditions.SetCondition(*condition)
+	case *regv1.ExternalRegistry:
+		p.Status.Conditions.SetCondition(*condition)
 	}
 }
 
